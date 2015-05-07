777137e4d35d37ee1dfee3bbfc1211fc483f7e70
Lewis Wright <LewisW@users.noreply.github.com>
2015-05-07 16:59:08 +0100

    Update docker-command.sh

--- docker-command.sh
+++ docker-command.sh
@@ -9,32 +9,23 @@ VOLUME=
 SHOULD_OUTPUT=0
 INTERACTIVE=
 
-#if [ "$PHPCI_BUILD" = "" ]; then
-#       PHPCI_BUILD="$BUILD_NUMBER"             
-#       PHPCI_BUILD_PATH="$PWD"
-#fi
-
 while getopts "u:sd:b:" OPTION
 do
      case $OPTION in
          u)
-             export USER="$OPTARG"
+               export USER="$OPTARG"
              ;;
          s)
-             NET="--net container:selenium-$GIT_COMMIT"
+              echo "*** Starting Selenium";
+              selenium=`docker run --privileged --rm -p 5999:6999 -d lewisw/selenium
+              NET="--net container:$selenium"
              ;;
-#         o)
-#            SHOULD_OUTPUT=1
-#             ;;
          d)
                 WORKSPACE="$OPTARG"
              ;;
          b)
                 GIT_COMMIT="$OPTARG"
              ;;
-#         f)
-#             OUTPUT_FILE="$OPTARG"
-#             ;;
      esac
 done
 
@@ -43,33 +34,18 @@ shift $((OPTIND-1))
 
 LOG_DIR=$(basename $0)
 
-#if [ "$SHOULD_OUTPUT" -eq 1 ];
-#then
-#        tmpfile="/tmp/docker-run-output.tmp"
-#       
-#       tmpdir=$(mktemp -d)
-#       trap 'rm -rf "$tmpdir"' EXIT INT TERM HUP
-#        hosttmp="$tmpdir/pipe"
-#       mkfifo "$hosttmp"       
-# 
-#        VOLUME="-v $hosttmp:$tmpfile"
-#fi
-
 if [ -t 1 ];
 then
         INTERACTIVE=' -it'
 fi
 
-command="docker run $INTERACTIVE --rm --privileged $VOLUME -v $WORKSPACE/build/$LOG_DIR:/project/build/logs -v $WORKSPACE/build/$LOG_DIR:/project/app/logs $NET build-$GIT_COMMIT --"
+command="docker run $INTERACTIVE --rm --privileged $VOLUME -v $WORKSPACE/build/$LOG_DIR:/project/build/logs -v $WORKSPACE/build/$LOG_DIR:/project/app/logs $NET build-$GIT_COMMIT -- $@"
 
-#if [ "$tmpfile" != "" ];
-#then
-#        command="$command logsave $tmpfile $@"
-#       $command > /dev/null 2>&1 &
-#
-#       cat $hosttmp
-#else
-        command="$command $@"
-        echo "*** Running command: $command";
-        $command
-#fi
+echo "*** Running command: $command";
+$command
+
+if [ -n "$selenium" ];
+then
+     docker stop $selenium
+     echo "*** Stoping Selenium";
+fi
