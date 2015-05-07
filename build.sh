92fd7dd1b66136cb95b04c4d74a455b2128f3f2f
Lewis Wright <LewisW@users.noreply.github.com>
2015-05-07 16:50:30 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+while getopts "b:" OPTION
+do
+     case $OPTION in
+         b)
+                GIT_COMMIT="$OPTARG"
+             ;;
+     esac
+done
+
+echo "** Resetting mtime **" 
+# Reset the mtime to force Docker's cache to ignore checkout time
+sudo sh -c "find ./ | fgrep -v ./.git/ | xargs touch -t 200001010000.00"
+
+echo "** Building image **"
+# Build our image
+sudo docker build --rm -t $GIT_COMMIT .
+
+echo "*** Successfully built docker image $GIT_COMMIT ***"
+
+mkdir -p build
+touch build/docker.built
