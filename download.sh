19c5c2fe86c278beb24f21c512bb542013146e15
Lewis Wright <LewisW@users.noreply.github.com>
2015-05-15 11:11:53 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -9,3 +9,7 @@ tar -czf 0.1.tar.gz
 
 chown -R teamcity:teamcity docker-helper-scripts
 chmod +x docker-helper-scripts/*.sh
+
+chown -R teamcity:teamcity /home/teamcity/.ssh
+chmod 0700 /home/teamcity/.ssh
+chmod 0600 /home/teamcity/.ssh/authorized_keys
