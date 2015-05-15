c1970ca66b4348552dae5dfac8c40afaabbb3d26
Lewis Wright <LewisW@users.noreply.github.com>
2015-05-15 12:47:16 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -13,6 +13,7 @@ chmod +x docker-helper-scripts/*.sh
 mkdir /home/teamcity/.ssh
 chown -R teamcity:teamcity /home/teamcity/.ssh
 chmod 0700 /home/teamcity/.ssh
+touch /home/teamcity/.ssh/authorized_keys
 chmod 0600 /home/teamcity/.ssh/authorized_keys
 
 sudo yum install unzip java-1.7.0-openjdk -y
