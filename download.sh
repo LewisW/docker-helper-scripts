b27590290025192e75ee096fce044de4e55caf8a
Lewis Wright <LewisW@users.noreply.github.com>
2015-05-15 18:31:43 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,9 +3,12 @@
 useradd teamcity
 gpasswd -a teamcity docker
 
+yum install unzip java-1.7.0-openjdk php php-cli git -y
+
 cd /home/teamcity
-curl -O -L https://github.com/LewisW/docker-helper-scripts/archive/0.1.tar.gz
-tar -czf 0.1.tar.gz
+git clone https://github.com/LewisW/docker-helper-scripts.git docker-helper-scripts
+#curl -O -L https://github.com/LewisW/docker-helper-scripts/archive/0.1.tar.gz
+#tar -czf 0.1.tar.gz
 
 chown -R teamcity:teamcity docker-helper-scripts
 chmod +x docker-helper-scripts/*.sh
@@ -19,6 +22,6 @@ chown -R teamcity:teamcity /home/teamcity/.ssh
 chmod 0700 /home/teamcity/.ssh
 chmod 0600 /home/teamcity/.ssh/authorized_keys
 
-sudo yum install unzip java-1.7.0-openjdk -y
+curl https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer
 
 echo 'teamcity ALL = NOPASSWD: ALL' >> /etc/sudoers
