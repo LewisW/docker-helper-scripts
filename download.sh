bedd2ea9d4a5e71c46fbac2c073c80987c4a8d8f
Lewis Wright <LewisW@users.noreply.github.com>
2015-05-26 17:05:23 +0100

    Removed __ and changed to variables, added line to download images off registry

--- download.sh
+++ download.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+TUNNEL=
+APT_CACHER=TEAMCITY=DOCKER=
+OAUTH_KEY=
+PUBLIC_KEY=
+
 useradd teamcity
 gpasswd -a teamcity docker
 
@@ -16,16 +21,19 @@ chmod +x docker-helper-scripts/*.sh
 mkdir /home/teamcity/.ssh
 
 #touch /home/teamcity/.ssh/authorized_keys
-echo "ssh-rsa __KEY_HERE__" >> /home/teamcity/.ssh/authorized_keys
+echo "ssh-rsa $PUBLIC_KEY" >> /home/teamcity/.ssh/authorized_keys
 
 chown -R teamcity:teamcity /home/teamcity/.ssh
 chmod 0700 /home/teamcity/.ssh
 chmod 0600 /home/teamcity/.ssh/authorized_keys
 
 curl https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer
-composer config -g github-oauth.github.com __YOUR_OAUTH_KEY__
+composer config -g github-oauth.github.com $OAUTH_KEY
 
-echo 'ssh -f tunnel@__TUNNEL__ -L 8111:__APT_CACHER__:8111 -L 3142:__APT_CACHER__:3142 -N' >> /home/teamcity/.bashrc
-echo 'git pull' >> /home/teamcity/.bashrc
-echo '127.0.0.1 __APT_CACHER__' >> /etc/hosts
-echo 'teamcity ALL = NOPASSWD: ALL' >> /etc/sudoers
+echo "ssh -f tunnel@$TUNNEL -L 8111:$TEAMCITY:8111 -L 5000:$TEAMCITY:5000 -L 3142:$APT_CACHER:3142 -N" >> /home/teamcity/.bashrc
+echo "git pull" >> /home/teamcity/.bashrc
+echo "127.0.0.1 $APT_CACHER $TEAMCITY $DOCKER" >> /etc/hosts
+echo "teamcity ALL = NOPASSWD: ALL" >> /etc/sudoers
+
+# Download all build images
+docker search localhost:5000/build | awk '{print $1}' | xargs -L1 docker pull
