3f70054443eca076497f93d30b7c5bd6f2f2488e
Lewis Wright <LewisW@users.noreply.github.com>
2015-07-08 17:11:27 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -28,7 +28,10 @@ chmod 0700 /home/teamcity/.ssh
 chmod 0600 /home/teamcity/.ssh/authorized_keys
 
 curl https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer
+# Run as teamcity:
 composer config -g github-oauth.github.com $OAUTH_KEY
+
+# Put tunnel private key in /etc/ssh/id_rsa
 
 echo "ssh -i /etc/ssh/id_rsa -f tunnel@$TUNNEL -L 8111:$TEAMCITY:8111 -L 5000:$TEAMCITY:5000 -L 3142:$APT_CACHER:3142 -N" >> /etc/rc.local
 echo "cd /home/teamcity/docker-scripts/ && git reset --hard HEAD && git pull && chown -R teamcity:teamcity . && chmod +x ./*.sh" >> /etc/rc.local
