3ea45d7ee12cfbdd7f31b8db4fb27b504153a442
Lewis Wright <LewisW@users.noreply.github.com>
2015-07-14 11:57:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,12 +17,12 @@ sudo sh -c "find ./ | fgrep -v ./.git/ | xargs touch -t 200001010000.00"
 echo "** Building image **"
 
 # Build our image
-sudo docker build --rm -t $GIT_COMMIT .
+sudo docker build --rm -t localhost:5000/$GIT_COMMIT .
 
 echo "*** Successfully built docker image $GIT_COMMIT ***"
 
 # Tag the image for the local repository
-sudo docker build -q --rm -t localhost:5000/$GIT_COMMIT . > /dev/null
+#sudo docker build -q --rm -t localhost:5000/$GIT_COMMIT . > /dev/null
 
 echo "*** Pushing to local repository ***"
 
