24b32fc1481c87338f835c011e6c46ca424e7cd3
Lewis Wright <LewisW@users.noreply.github.com>
2015-07-14 12:12:54 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -12,13 +12,13 @@ set -xe
 
 ssh-keyscan -H $TUNNEL > /etc/ssh/ssh_known_hosts
 
+yum update -y
+yum install docker unzip java-1.7.0-openjdk php php-cli git jq nc.x86_64 -y
+
 # Format the drive for direct-lvm devicemapper
 curl -O https://gist.githubusercontent.com/ambakshi/ddebac9148b4aea36446/raw/3954d97f367c05d41ae70791767afb74f65360d0/docker-direct-lvm.sh
 chmod +x docker-direct-lvm.sh
 ./docker-direct-lvm.sh /dev/xvdb
-
-yum update -y
-yum install docker unzip java-1.7.0-openjdk php php-cli git jq nc.x86_64 -y
 
 # Configure docker for the direct-lvm
 #echo "--storage-opt dm.datadev=/dev/direct-lvm/data --storage-opt dm.metadatadev=/dev/direct-lvm/metadata" > /etc/sysconfig/docker
