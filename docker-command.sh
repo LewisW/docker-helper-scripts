2821e38c2fa6f6369c92d3d1b039092983f69ca1
Lewis Wright <LewisW@users.noreply.github.com>
2015-07-22 09:39:32 +0100

    Changed image to skip runit and startup files.

--- docker-command.sh
+++ docker-command.sh
@@ -17,31 +17,47 @@ GIT_COMMIT="$BUILD_NUMBER"
 NET=
 VOLUME=
 INTERACTIVE=
+BOOTOPTS="--skip-runit"
 
-while getopts "u:sSd:b:" OPTION
+while getopts "u:sSd:b:c" OPTION
 do
      case $OPTION in
+          # Force runit (for the cron and syslogs)
+         c)
+               BOOTOPTS=""
+             ;;
+          # Set the user
          u)
                export USER="$OPTARG"
              ;;
+          # Boot Selenium
          s)
               echo "*** Starting Selenium";
               selenium=`docker run --privileged -P -d lewisw/selenium-stable`
               NET="--net container:$selenium"
              ;;
+          # Boot Sauce Connect
          S)
               echo "*** Starting Sauce Connect";
               selenium=`docker run -d lewisw/docker-sauce-connect $SAUCE_USERNAME $SAUCE_ACCESS_KEY`
               NET="--net container:$selenium"
              ;;
+          # Use a different directory
          d)
                 WORKSPACE="$OPTARG"
              ;;
+          # Branch name
          b)
                 GIT_COMMIT="$OPTARG"
              ;;
      esac
 done
+
+# No selenium? Skip startup files
+if [ "$selenium" == "" ];
+then
+     BOOTOPTS="$BOOTOPTS --skip-startup-files"
+fi
 
 # Shift all processed options away
 shift $((OPTIND-1))
@@ -53,7 +69,7 @@ then
         INTERACTIVE=' -it'
 fi
 
-command="docker run $INTERACTIVE --rm --privileged $VOLUME -v $WORKSPACE/build/$LOG_DIR:/project/build/logs -v $WORKSPACE/build/$LOG_DIR:/project/app/logs $NET $GIT_COMMIT -- $@"
+command="docker run $INTERACTIVE --rm --privileged $VOLUME -v $WORKSPACE/build/$LOG_DIR:/project/build/logs -v $WORKSPACE/build/$LOG_DIR:/project/app/logs $NET $GIT_COMMIT $BOOTOPTS -- $@"
 
 echo "*** Running command: $command";
 $command
