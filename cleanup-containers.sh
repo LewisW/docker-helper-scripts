a6cd2b0fc756bc1a33d42d39a8696f39305530fd
Lewis Wright <LewisW@users.noreply.github.com>
2015-12-16 10:52:02 +0000

    Filtered the removal to just images in the registry

--- cleanup-containers.sh
+++ cleanup-containers.sh
@@ -22,10 +22,13 @@ then
         # Delete all images
         docker rmi $FORCE $(docker images -q)
 else
+        # Stop all leftover selenium containers
+        docker ps -a | grep 'selenium-stable' | awk '{print $1 }' | xargs -t --no-run-if-empty docker stop $FORCE
+        
         # Delete stopped containers
-        docker ps -a | grep Exited | awk '{print $1 }' | xargs -t --no-run-if-empty docker rm $FORCE
+        docker ps -a | grep 'build:' | grep Exited | awk '{print $1 }' | xargs -t --no-run-if-empty docker rm $FORCE
         # Delete old containers
-        docker ps -a | grep 'months\|weeks\|days ago' | awk '{print $1}' | xargs -t --no-run-if-empty docker rm $FORCE
+        docker ps -a | grep 'build:' | grep 'months\|weeks\|days ago' | awk '{print $1}' | xargs -t --no-run-if-empty docker rm $FORCE
         # Delete old images
-        docker images | grep 'months\|weeks\|[0-9]\{2,\} days ago' | awk '{print $3}' | xargs -t --no-run-if-empty docker rmi $FORCE
+        docker images | grep 'build:' | grep 'months\|weeks\|days ago' | awk '{print $3}' | xargs -t --no-run-if-empty docker rmi $FORCE
 fi
