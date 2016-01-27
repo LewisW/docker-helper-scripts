21ec3aa1d42520c029801788d6d78ca9f7fe03a3
Lewis Wright <LewisW@users.noreply.github.com>
2016-01-27 14:50:19 +0000

    Lowered the size of the database and disabled shared dir fix

--- docker-command.sh
+++ docker-command.sh
@@ -72,6 +72,8 @@ mkdir -p $WORKSPACE/logs/app
 
 command="docker run $INTERACTIVE \
 --privileged $VOLUME \
+-e NO_SHARED_DIRS=1 \
+-e MYSQL_RAM_SIZE=256 \
 -v $WORKSPACE/logs/build:/project/build/logs \
 -v $WORKSPACE/logs/app/:/project/app/logs \
 -v $WORKSPACE/app/cache:/project/app/cache \
